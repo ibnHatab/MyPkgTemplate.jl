9944dab2c2558a5a1f0c26860e75c3084624bffc
Documenter.jl <documenter@juliadocs.github.io>
2022-09-11 08:36:31 +0000

    build based on 5a4356f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MyPkgTemplate","category":"page"},{"location":"#MyPkgTemplate","page":"Home","title":"MyPkgTemplate","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyPkgTemplate.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MyPkgTemplate]","category":"page"}]
+}
