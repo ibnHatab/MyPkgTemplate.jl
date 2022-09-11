63aa5063fb046956d51f5cf9b910b5c21889f3f3
ibnHatab <lib.aca55a@gmail.com>
2022-09-11 12:10:54 +0300

    prepare docs

--- src/graph_functions.jl
+++ src/graph_functions.jl
@@ -1,5 +1,26 @@
 
+using VegaLite
 
+"""
+    sum_values(x, y)
+
+Sum the numeric arguments
+
+Maybe not only numeric.
+
+# Arguments
+* `x`: first adjunct
+* `y`: second adjunct
+
+# Note
+* can go wrong
+
+#Example
+```julia
+julia> sum_values(2, 2)
+4
+```
+"""
 function sum_values(x, y)
     return x + y
 end
